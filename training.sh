ee953e297a91a5005bd82bc5971ef237f4eea56d
Joel Tsuchitori <jtsuchit@plai12.cs.ubc.ca>
2024-02-12 15:50:49 -0800

    training script

---
+++ training.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /ubc/cs/home/j/jtsuchit/audio-diffuion/diffusion-test/bin/activate
+#pip install torch
+#pip install torchaudio
+#pip install wandb
+#pip install scipy
+python3 /ubc/cs/home/j/jtsuchit/audio-diffuion/test_training_run.py
